3c50c0ede76e1e3b68284affd247096e6310959f
FrancaTM <francatm@outlook.com>
2019-09-13 22:30:05 -0300

    modify get concrete number trivia to extend base class

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -25,7 +25,7 @@ void main() {
     when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
         .thenAnswer((_) async => Right(tNumberTrivia));
     // act
-    final result = await usecase(number: tNumber);
+    final result = await usecase(Params(number: tNumber));
     // assert
     expect(result, Right(tNumberTrivia));
     verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
