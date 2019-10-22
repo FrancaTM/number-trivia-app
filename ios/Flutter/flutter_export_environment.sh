75f8ab53a343d62fa8c7d9f654889ad5bbd44a45
FrancaTM <francatm@outlook.com>
2019-10-22 17:12:52 -0300

    inject dependencies into project

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/francatm/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/francatm/Documents/flutter_projects/number_trivia_app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/francatm/Documents/flutter_projects/number_trivia_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/francatm/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
